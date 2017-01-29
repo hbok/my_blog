987196c2d05c0e5ead98ac91f82611d4e7a2cbb1
Hiroki OKA <oka@i2m.jp>
2017-01-29 08:25:03 +0000

    リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,42 +1,44 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
     @posts = Post.all
     @new_posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new # フォーム用の空のインスタンスを生成する。
+    @post = Post.new
   end
 
   def create
-    @post = Post.new(post_params) # ストロングパラメータを引数に
-    @post.save # saveをしてデータベースに保存する。
-    redirect_to "/posts/#{@post.id}" # showページにリダイレクト
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
+    @post = Post.new(post_params)
+    @post.save
     redirect_to "/posts/#{@post.id}"
   end
 
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
+
+  end
+
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to "/posts"
   end
-  
+
   private
 
-  def post_params # ストロングパラメータを定義する
+  def post_params
     params.require(:post).permit(:title, :body, :category)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
