37cfbc2553f64f2c1caee50999c7b0fc362c44b7
Hiroki OKA <oka@i2m.jp>
2017-01-29 08:44:55 +0000

    検索作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
-    @posts = Post.order(created_at: :desc)
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result(distinct: true)
     # 最新記事を５つ取得
     @new_posts = Post.order(created_at: :desc).limit(5)
   end
